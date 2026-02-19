d77fc4eb049ec109e2f32617257d0d1f6dc583da
codedev1 <coderx543@gmail.com>
2026-02-19 17:26:26 +1300

    fix 2

--- websocket/tailwind.config.js
+++ websocket/tailwind.config.js
@@ -1,7 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./*.html",
+    "./index.html",
+    "./website/**/*.html",
+    "./website/**/*.js"
   ],
   theme: {
     extend: {},
